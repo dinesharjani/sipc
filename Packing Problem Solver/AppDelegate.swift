d0aa990e6eb3cd02fafa50b2463ea79cc30927ab
Dinesh Harjani <dinesharjani@gmail.com>
2016-10-05 16:32:45 +0100

    First commit.

---
+++ Packing Problem Solver/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Packing Problem Solver
+//
+//  Created by Dinesh Harjani on 5/10/16.
+//  Copyright © 2016 Dinesh Harjani. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+}
+
---
+++ Packing Problem Solver/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Packing Problem Solver
+//
+//  Created by Dinesh Harjani on 5/10/16.
+//  Copyright © 2016 Dinesh Harjani. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+            // Update the view, if already loaded.
+        }
+    }
+}
+

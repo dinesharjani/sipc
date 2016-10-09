71e21f57786f3927c022e41c93cc93a238deb275
Dinesh Harjani <dinesharjani@gmail.com>
2016-10-09 16:21:47 +0100

    WIP: Drawing the strip. Currently draws something that looks like the strip's outline.

--- Packing Problem Solver/UI Classes/StripView.swift
+++ Packing Problem Solver/UI Classes/StripView.swift
@@ -16,13 +16,41 @@ class StripView: NSView {
 		}
 	}
 	
+	let margin = Float(0.1)
+	
     override func draw(_ dirtyRect: NSRect) {
         super.draw(dirtyRect)
 
 		guard (strip != nil) else {
 			return;
 		}
-        // Drawing code here.
+		
+		let xOffset = Int(margin * Float(strip!.width))
+		let yOffset = Int(margin * Float(strip!.height))
+		
+		let widthAspectRatio = (Int(dirtyRect.size.width) + 2 * xOffset) / strip!.width
+		let heightAspectRatio = (Int(dirtyRect.size.height) + 2 * yOffset) / strip!.height
+		
+		let xStart = xOffset * widthAspectRatio
+		let xEnd =  strip!.width * widthAspectRatio
+		let yStart = yOffset * heightAspectRatio
+		let yEnd = strip!.height * heightAspectRatio
+		
+		let blackColor = NSColor.black
+		blackColor.set()
+		
+		let stripOutlinePath = NSBezierPath(rect: dirtyRect)
+		stripOutlinePath.move(to: NSMakePoint(CGFloat(xStart), CGFloat(yStart)))
+		stripOutlinePath.line(to: NSMakePoint(CGFloat(xStart), CGFloat(yEnd)))
+		stripOutlinePath.move(to: NSMakePoint(CGFloat(xStart), CGFloat(yStart)))
+		stripOutlinePath.line(to: NSMakePoint(CGFloat(xEnd), CGFloat(yStart)))
+		stripOutlinePath.line(to: NSMakePoint(CGFloat(xEnd), CGFloat(yEnd)))
+		stripOutlinePath.stroke()
+		
+		
+		let i = 5
+		
+		
     }
     
 }

be83c7dfd6568d7416c790d2ac428308dbf4ed14
Dinesh Harjani <dinesharjani@gmail.com>
2016-10-09 15:54:16 +0100

    Bug fix: rectangle's fitsIn() function was not properly calculating the space required by the margins.

--- Packing Problem Solver/Data Classes/Rectangle.swift
+++ Packing Problem Solver/Data Classes/Rectangle.swift
@@ -31,6 +31,6 @@ class Rectangle: NSObject {
 	}
 	
 	func fitsIn(width: Int, height: Int) -> Bool {
-		return (self.width + margin) <= width && (self.height + margin) <= height;
+		return (self.width + 2 * margin) <= width && (self.height + 2 * margin) <= height;
 	}
 }

e25a2ba9a2a17ad5e7ee976d0b7f20f69895f348
Dinesh Harjani <dinesharjani@gmail.com>
2016-10-26 17:17:27 +0100

    Experiment class compiles, but it's not being used yet.

--- Packing Problem Solver/Algorithms/Experiment.swift
+++ Packing Problem Solver/Algorithms/Experiment.swift
@@ -10,14 +10,29 @@ import Cocoa
 
 class Experiment: NSObject {
 
-	let TimerInterval = 1
+	private let TimerInterval = 1
 	
-	let problem: Problem
-	let algorithm: HeuristicAlgorithm
-	let timeLimit: Int
-	let numberOfThreads: Int
+	var bestSolution: BaseStrip?
 	
-	var timer: Timer
+	private var callbackQueue: DispatchQueue {
+		return DispatchQueue.global(qos: DispatchQoS.QoSClass.userInteractive)
+	}
+	private var controlQueue: DispatchQueue {
+		return DispatchQueue(label: "com.packagesolver.experiment.control")
+	}
+	private var computationalQueue: DispatchQueue {
+		return DispatchQueue.global(qos: DispatchQoS.QoSClass.utility)
+	}
+	
+	private let problem: Problem
+	private let algorithm: HeuristicAlgorithm
+	private let timeLimit: Int
+	private let numberOfThreads: Int
+	
+	private var timer: Timer
+	private var accumulatedTime: Int
+	private var finished: Bool
+	private var callback: ((_ elapsed: Int, _ finished: Bool) -> Void)?
 	
 	init (problem: Problem, algorithm: HeuristicAlgorithm, timeLimit: Int, numberOfThreads: Int) {
 		self.problem = problem
@@ -25,9 +40,58 @@ class Experiment: NSObject {
 		self.timeLimit = timeLimit
 		self.numberOfThreads = numberOfThreads
 		self.timer = Timer()
+		accumulatedTime = 0
+		finished = false
 	}
 	
-	func run() {
-		// No-op.
+	func run(callback: @escaping (_ elapsed: Int, _ finished: Bool) -> Void) {
+		self.callback = callback
+		self.timer.invalidate()
+		finished = false
+		
+		accumulatedTime = 0
+		timer = Timer.scheduledTimer(timeInterval: TimeInterval(TimerInterval), target: self, selector: #selector(timerAction), userInfo: nil, repeats: true)
+		for _ in 1...numberOfThreads {
+			computationalQueue.async(execute: solve)
+		}
+	}
+	
+	func timerAction() {
+		controlQueue.async { 
+			self.accumulatedTime += self.TimerInterval
+			if (self.accumulatedTime >= self.timeLimit) {
+				self.timer.invalidate()
+				self.finished = true
+			}
+			
+			self.callback!(self.accumulatedTime, self.finished)
+		}
+	}
+	
+	private func solve() {
+		while (!finished) {
+			do {
+				let solutionOrder = algorithm.solve(problem: problem)
+				let solution = try problem.applySolution(solution: solutionOrder)
+				
+				guard (!finished) else {
+					return
+				}
+				
+				controlQueue.sync {
+					guard (bestSolution != nil) else {
+						bestSolution = solution
+						return;
+					}
+					
+					if (bestSolution!.height > solution.height
+						|| bestSolution!.totalEmptySpacesArea() > solution.totalEmptySpacesArea()) {
+						bestSolution = solution
+					}
+				}
+			} catch {
+				// try again
+			}
+		}
 	}
 }

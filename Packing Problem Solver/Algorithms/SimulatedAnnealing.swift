767f937fb75f8cbb5f862bb266c66130d545d4a0
Dinesh Harjani <dinesharjani@gmail.com>
2016-11-09 18:30:23 +0000

    A bunch of code that looks like a SA algorithm is implemented and seems to run, but isn’t finished; for example, we haven’t implemented the variable change in the number of iterations that can be done.

--- Packing Problem Solver/Algorithms/SimulatedAnnealing.swift
+++ Packing Problem Solver/Algorithms/SimulatedAnnealing.swift
@@ -10,7 +10,54 @@ import Cocoa
 
 class SimulatedAnnealing: HeuristicAlgorithm {
 	
+	private static let MaxNumberOfSteps = 5
+	private static let StartingTemperature = 29.87
+	private static let CoolingFactor = 0.877
+	
 	override func solve(problem: Problem) -> [Int] {
-		return Algorithms.HillClimbingAlgorithm.instance().solve(problem: problem)
+		var bestSolution: [Int]
+		
+		do {
+			bestSolution = Algorithms.RandomAlgorithm.instance().solve(problem: problem)
+			var bestStrip = try problem.applySolution(solution: bestSolution)
+			
+			var temperature = SimulatedAnnealing.StartingTemperature
+			print(String(format: "Temperature: %.2f°C", temperature))
+			
+			repeat {
+				for _ in 1...SimulatedAnnealing.MaxNumberOfSteps {
+					let neighborSolution = neighbor(startingSolution: bestSolution, currentTemperature: temperature)
+					let neighborStrip = try problem.applySolution(solution: neighborSolution)
+					
+					if (neighborStrip.isBetterThan(otherStrip: bestStrip)
+						|| acceptWorseSolution(currentValue: bestStrip.guidanceValue(), proposedValue: neighborStrip.guidanceValue(), temperature: temperature)) {
+						bestSolution = neighborSolution
+						bestStrip = neighborStrip
+					}
+				}
+				
+				temperature *= SimulatedAnnealing.CoolingFactor
+				print(String(format: "Temperature: %.2f°C", temperature))
+			} while (temperature >= 1)
+		}
+		catch {
+			// Keep going.
+		}
+		
+		return bestSolution
+	}
+	
+	private func neighbor(startingSolution: [Int], currentTemperature: Double) -> [Int] {
+		return Movement.OneExchange.performMovement(initialSolution: startingSolution)
+	}
+	
+	private func acceptWorseSolution(currentValue: Int, proposedValue: Int, temperature: Double) -> Bool {
+		let p = abs(Double(currentValue) - Double(proposedValue)) / temperature
+		let r = randomNumberBetweenZeroAndOne()
+		return exp(p) > r
+	}
+	
+	private func randomNumberBetweenZeroAndOne() -> Double {
+		return Double(arc4random()) / Double(UInt32.max)
 	}
 }
